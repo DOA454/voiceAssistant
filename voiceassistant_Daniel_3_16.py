8fc5632784b234a9c550865370fcdec0246167c6
DOA454 <danielaudi75@gmail.com>
2023-04-19 20:57:14 -0500

    Added a class for the assistant

--- voiceassistant_Daniel_3_16.py
+++ voiceassistant_Daniel_3_16.py
@@ -1,4 +1,3 @@
-import subprocess
 import random
 import speech_recognition as sr # convert speech to text
 import datetime # for fetching date and time
@@ -9,6 +8,7 @@ import playsound # to play saved mp3 file
 from gtts import gTTS # google text to speech
 import os # to save/open files
 import wolframalpha # to calculate strings into formula
+import pyttsx3 as tts
 from selenium import webdriver # to control browser operations
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -18,75 +18,79 @@ from selenium.webdriver.edge.options import Options
 import time
 import pyautogui
 import win32com.client
+import threading
 #GUI 
 from tkinter import *
 from tkinter.ttk import*
+from IPython.display import Markdown
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
-def create_gui():
-    window = Tk() # create object
-    window.title('Team 4 Voice Assistant Demo')
+
+   
+
+class Jarvis:
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+        self.speaker = tts.init()
+
+        threading.Thread(target=main).start()
+        
+        self.window = Tk()
+
+
+
+def create_gui(self):
+    
+    self.window.title('Team 4 Voice Assistant Demo')
    # Set window size
-    window.geometry('500x500')
-    window.configure(background='white')
+    self.window.geometry('500x500')
+    self.window.configure(background='white')
 
 
-    style = Style()
-    style.configure('W.TButton', font =
+    self.style = Style()
+    self.style.configure('W.TButton', font =
                 ('calibri', 10, 'bold', 'underline'),
                     foreground = 'green')
 
-    button1 = Button(window, text = 'HELLO', command = main , style='W.TButton')
-    button1.grid(row = 5, column = 3, pady = 10, padx = 100)
-    button1.place(relx=0.5, rely=0.3, anchor=CENTER)
+    self.button1 = Button(self.window, text = 'HELLO', command = main , style='W.TButton')
+    self.button1.grid(row = 5, column = 3, pady = 10, padx = 100)
+    self.button1.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-    style.configure('S.TButton', font =
+    self.style.configure('S.TButton', font =
                 ('calibri', 10, 'bold', 'underline'),
                     foreground = 'red')
 
-    button2 = Button(window, text = 'GOODBYE', command = stop, style='S.TButton')
-    button2.grid(row = 6, column = 3, pady = 10, padx = 100)
-    button2.place(relx=0.5, rely=0.5, anchor=CENTER)
+    self.button2 = Button(self.window, text = 'GOODBYE', command = stop, style='S.TButton')
+    self.button2.grid(row = 6, column = 3, pady = 10, padx = 100)
+    self.button2.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    label_font = ('Open Sans', 12)
-    label1 = Label(window, text='Virtual Assistant : Jarvis', font = label_font)
-    label2 = Label(window, text='Welcome, How can I help you?', font = label_font)
-    label1.pack()
-    label2.pack()
+    self.label_font = ('Open Sans', 12)
+    self.label1 = Label(self.window, text='Virtual Assistant : Jarvis', font = self.label_font)
+    self.label2 = Label(self.window, text='Welcome, How can I help you?', font = self.label_font)
+    self.label1.pack()
+    self.label2.pack()
 
-    window.mainloop()
-   
+    self.window.mainloop()
 
 
 
 
 # Capture voice input from user and uses speech_recognition library to capture words spoken as text
-def talk():
-    input = sr.Recognizer()
+def talk(self):
     with sr.Microphone() as source:
         # input.adjust_for_ambient_noise(source)
-        audio = input.listen(source)
-        data = ""
+        audio = self.recognizer.listen(source)
         try:
-            data = input.recognize_google(audio)
+            data = self.recognizer.recognize_google(audio)
             print("You said, " + data)
 
         except sr.UnknownValueError:
-            respond("Cannot recognize speech")
+            self.speaker.say("Cannot recognize speech")
 
         except sr.RequestError:
-            respond("Speech recognition failed. Check your internet connection or API key")
+            self.speaker.say("Speech recognition failed. Check your internet connection or API key")
 
     return data
-
-# Voice assistant responding to user using the playsound library. Writes the speech recognized to a file,
-# uses Playsound to read out the file and then deletes the file from the system. 
-def respond(output):
-    print(output)
-    response=gTTS(text=output, lang='en', tld="com.au")
-    file = "audio.mp3"
-    response.save(file)
-    playsound.playsound(file)
-    os.remove(file)
 
 # -------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------
@@ -96,24 +100,24 @@ def respond(output):
 
 # Email tasks
 # -------------------------------------------------------------------------------------------------
-def email_draft():
+def email_draft(self):
     
     outlook = win32com.client.Dispatch('Outlook.application')
     mail = outlook.createItem(0)
     
-    respond("Who would you like to send the email to?")
+    self.speaker.say("Who would you like to send the email to?")
     recipient = talk().replace(" ","")
     if recipient == 0:
-        respond('Please repeat')
+        self.speaker.say('Please repeat')
         recipient = talk().replace(" ", "")
-    respond("What is the subject of your email?")
+    self.speaker.say("What is the subject of your email?")
     subject = talk()
     if subject is None:
-        respond('The email will be sent without a subject')
-    respond("What would you like the email to say?")
+        self.speaker.say('The email will be sent without a subject')
+    self.speaker.say("What would you like the email to say?")
     body = talk()
     if body == "stop":
-        respond("Aborting process")
+        self.speaker.say("Aborting process")
         main()
     
 
@@ -124,30 +128,30 @@ def email_draft():
     try:
         mail.save()
     except:
-        respond('Something went wrong. Lets try this again')
+        self.speaker.say('Something went wrong. Lets try this again')
         email_draft()
     return mail
 
-def email_send():
+def email_send(self):
     mail = email_draft()
     try:
         mail.Send()
-        respond('Email sent successfully')
+        self.speaker.say('Email sent successfully')
     except:
-        respond("Email did not send, let's start over")
+        self.speaker.say("Email did not send, let's start over")
         email_send()
 
 
-def open_email():
-    respond("Opening Outlook email")
+def open_email(self):
+    self.speaker.say("Opening Outlook email")
     os.startfile('outlook')
     
 # -------------------------------------------------------------------------------------------------
 # Weather
 # -------------------------------------------------------------------------------------------------
-def weather(city):
+def weather(self, city):
     api_key = '38af91f97ea3a0243ec6cb45019bfb4d'
-    # respond("Which city?")
+    # self.speaker.say("Which city?")
     # city = talk().lower()
 
     if city != 0:
@@ -161,71 +165,71 @@ def weather(city):
             temp = data["main"]["temp"]
             feels_like = data["main"]["feels_like"]
             description = data["weather"][0]["description"]
-            respond(f"Current weather in {city}: {description}. Temperature: {temp}°C. Feels like: {feels_like}°C.")
+            self.speaker.say(f"Current weather in {city}: {description}. Temperature: {temp}°C. Feels like: {feels_like}°C.")
         else:
             print(f"Error retrieving weather data. Error code: {response.status_code}")
 
 # Opening applications
 # -------------------------------------------------------------------------------------------------
-def open_word():
+def open_word(self):
     os.startfile("WINWORD.EXE")
-    respond("Opening Microsoft Word")
+    self.speaker.say("Opening Microsoft Word")
     time.sleep(2)
-    respond("What do you want to write?")
+    self.speaker.say("What do you want to write?")
     content = talk()
     shell = win32com.client.Dispatch("WScript.Shell")
     shell.SendKeys(content)
     save_file()
 
 
-def open_notepad():
+def open_notepad(self):
 	os.startfile("notepad.exe")
-	respond('Opening notepad')
+	self.speaker.say('Opening notepad')
 	time.sleep(2)
-	respond('what would you like to write')
+	self.speaker.say('what would you like to write')
 	content = talk().lower()
 	pyautogui.write(content)
 	save_file()
         
-def save_file():
+def save_file(self):
     pyautogui.hotkey('ctrl','s')
     time.sleep(1)
-    respond('What name would you like to put for the file?')
+    self.speaker.say('What name would you like to save the file as?')
     new_filename =talk().lower()
     pyautogui.write(new_filename) # enter the file name
     pyautogui.press('enter') # confirm the save
-    respond(f'file saved as: {new_filename}')
+    self.speaker.say(f'file saved as: {new_filename}')
 
-def close_app(app_name):    
+def close_app(self, app_name):    
     if app_name != 0:
-        respond(f"Closing {app_name}")
+        self.speaker.say(f"Closing {app_name}")
         try:
             os.system(f"taskkill /f /im {app_name}.exe")
         except:
-            respond(f"Unable to close {app_name}")
+            self.speaker.say(f"Unable to close {app_name}")
 
     # else:
-    #     respond(f"Unable to find {app_name} ") 
+    #     self.speaker.say(f"Unable to find {app_name} ") 
 
 # -------------------------------------------------------------------------------------------------
 # Internet search
 # -------------------------------------------------------------------------------------------------
 # Using webbrowser
-def google():
+def google(self):
     webbrowser.open_new_tab("https://www.google.com")
-    respond("Google is open")
+    self.speaker.say("Google is open")
     
 # Using wikipedia library
-def wiki(text):
-    respond('Searching Wikipedia')
+def wiki(self, text):
+    self.speaker.say('Searching Wikipedia')
     text = text.replace("wikipedia", "")
     results = wikipedia.summary(text, sentences = 1)
-    respond("According to Wikipedia")
+    self.speaker.say("According to Wikipedia")
     print(results)
-    respond(results) 
+    self.speaker.say(results) 
 
 # Using selenium and edge webdriver to open webpages
-def get_webpage(url, text, sleep=5, retries=3):
+def get_webpage(self, url, text, sleep=5, retries=3):
     for i in range(1, retries+1):
         time.sleep(sleep * i)
 
@@ -235,7 +239,7 @@ def get_webpage(url, text, sleep=5, retries=3):
             page = browser.get(url)
             browser.implicitly_wait(3)
             browser.maximize_window()
-            respond("Opening in youtube")
+            self.speaker.say("Opening in youtube")
             indx = text.split().index('youtube')
             query = text.split()[indx + 1:]
             search = '+'.join(query)
@@ -251,54 +255,63 @@ def get_webpage(url, text, sleep=5, retries=3):
 # ----------------------------------------------------------------------------------------------------
 
 
-def calculate(question):
+def calculate(self, question):
     app_id="4YVHWL-29XPVVGXQP"
     client = wolframalpha.Client(app_id)
     res = client.query(question)
     answer = next(res.results).text
-    respond(f"The answer is {answer}")
+    self.speaker.say(f"The answer is {answer}")
 
 # System functions
 # ----------------------------------------------------------------------------------------------------
 # Time
-def tell_time():
+def tell_time(self):
     strTime=datetime.datetime.now().strftime("%H:%M:%S")
-    respond(f"the time is {strTime}") 
+    self.speaker.say(f"the time is {strTime}") 
 
 # Shutdown, logout, and restart
-def logout():
-    respond("Logging out of  computer")
+def logout(self):
+    self.speaker.say("Logging out of  computer")
     os.system("shutdown /l")
 
-def restart():
-    respond("Restarting computer")
+def restart(self):
+    self.speaker.say("Restarting computer")
     os.system("shutdown /g /t 30")
 
-def shutdown():    
-    respond("Shutting computer down")
+def shutdown(self):    
+    self.speaker.say("Shutting computer down")
     os.system("shutdown /s /t 30")
 
 # Stop program
 def stop():
     exit()
 
+def chatbot(self, query):
+    self.speaker.say(" Give me a sec")
+    os.environ["OPENAI_API_KEY"] = "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"
+    documents = SimpleDirectoryReader('read').load_data()
+    index = GPTSimpleVectorIndex(documents)
+
+    answer = index.query(query)
+    self.speaker.say(answer.response)
+
 # -------------------------------------------------------------------------------------------------
 # Main function
 # -------------------------------------------------------------------------------------------------
-def main():
+def main(self):
     greetings = ["I'm Jarvis, your desktop assistant, how can I help today?",
                  "I'm Jarvis, what's up?",
                  "Jarvis at your service",
                  "How can I help?",
                  "What can I do for you?"]
     choice =  random.choice(greetings)
-    respond(choice)
+    self.speaker.say(choice)
     text = talk().lower()
 
-    if text != 0:       
+    if text is not None:       
 
         if any(word in text for word in ["stop", "exit", "bye"]):
-            respond("Jarvis, out!")
+            self.speaker.say("Jarvis, out!")
             stop()
 
         elif 'wikipedia' in text:
@@ -307,7 +320,7 @@ def main():
         elif 'close' in text:
             app_name = text.replace("close ", "")
             close_app(app_name)
-            respond(f"{app_name} is closed")
+            self.speaker.say(f"{app_name} is closed")
             time.sleep(2)
 
         elif 'time' in text:
@@ -325,15 +338,6 @@ def main():
             google()
             time.sleep(5)
 
-        elif "save file" in text:
-            respond("Saving file")
-            try:
-                pyautogui.hotkey('ctrl','s')
-                time.sleep(1)
-            
-            except Exception as e:
-                respond(f"Error: {e}")
-
         elif 'youtube' in text:
             url = 'https://www.youtube.com/'
             get_webpage(url, text)
@@ -346,7 +350,6 @@ def main():
                     
         elif "weather" in text:
             city = text.replace("what's the weather in ","")
-            print(city)
             weather(city)
                 
         elif "shutdown computer " in text:
@@ -365,19 +368,24 @@ def main():
             email_draft()
 
         elif 'send email' in text:
-            respond('Opening email')
+            self.speaker.say('Opening email')
             email_send()
 
+        elif text != 0:
+            chatbot(text)
+
         else:
-            respond("I'm not programmed to help with that")
-            time.sleep(2)
+            self.speaker.say("Let's start over")
             main()
+            # self.speaker.say("I'm not programmed to help with that")
+            # time.sleep(2)
+            # main()
 
 
 
 
 # Driver code
 if __name__=='__main__':
-    create_gui()
+    Jarvis()
   
 
